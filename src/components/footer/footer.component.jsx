34ad7d9159740916e8e0c827c661b4302b12c392
Kristóf Kövér <kristof.kover@lightware.com>
2019-08-29 19:02:01 +0200

    Footer component created

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './footer.styles.scss';
+
+const Footer = () => (
+  <div className='footer'>
+    <div id='logo-licence'>
+      Icon made by&nbsp;
+      <a href='https://freepik.com/home'>Freepik</a>
+      &nbsp;from&nbsp;
+      <a href='http://www.flaticon.com/'>www.flaticon.com</a>
+    </div>
+  </div>
+);
+
+export default Footer;
