1d7639cc80819c4f5973ce7a9b63ea4a5b61425e
Kristóf Kövér <kkrisst@gmail.com>
2019-09-01 12:11:10 +0200

    Added the forms to sign up and sign in components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,11 +1,71 @@
 import React from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils.js';
 
 import './sign-in.styles.scss';
 
-const SignIn = () => (
-    <div className='sign-in'>
-        SIGN IN PLACEHOLDER
-    </div>
-);
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    onSubmit = async event => {
+        event.preventDefault();
+
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.log(error);
+        }
+
+        this.setState({ email: '', password: '' });
+    }
+
+    onInputChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>Sign in with an existing account</h2>
+
+                <form onSubmit={this.onSubmit}>
+                    <FormInput
+                        name="email"
+                        type="email"
+                        value={this.state.email}
+                        label="email"
+                        handleChange={this.onInputChange}
+                        required />
+                    <FormInput
+                        name="password"
+                        type="password"
+                        value={this.state.password}
+                        label="password"
+                        handleChange={this.onInputChange}
+                        required />
+
+                    <div className='buttons'>
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                    </div>
+                </form>
+            </div>
+        );
+      }
+};
 
 export default SignIn;
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,11 +1,110 @@
 import React from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
 import './sign-up.styles.scss';
 
-const SignUp = () => (
-    <div className='sign-up'>
-        SIGN UP PLACEHOLDER
-    </div>
-);
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+
+            passwordMismatchMessage: '',
+        }
+    }
+
+    onSubmit = async event => {
+        event.preventDefault();
+
+        const { displayName, email, password, confirmPassword } = this.state;
+
+        if(password !== confirmPassword) {
+            this.setState({
+                passwordMismatchMessage: 'The passwords you provided do not match.',
+            });
+            return;
+        } else if (password.length < 6) {
+            this.setState({
+                passwordMismatchMessage: 'The password should be at least 6 characters long.',
+            });
+            return;
+        }
+
+        try {
+        const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+        await createUserProfileDocument(user, { displayName });
+
+        this.setState({
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+        });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    onInputChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+        [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <div className='sign-up'>
+                <h2>Create a new account</h2>
+
+                <form onSubmit={this.onSubmit}>
+                    <FormInput
+                        name="displayName"
+                        type="text"
+                        value={this.state.displayName}
+                        label="display name"
+                        handleChange={this.onInputChange}
+                        required />
+                    <FormInput
+                        name="email"
+                        type="email"
+                        value={this.state.email}
+                        label="email"
+                        handleChange={this.onInputChange}
+                        required />
+                    <FormInput
+                        name="password"
+                        type="password"
+                        value={this.state.password}
+                        label="password"
+                        handleChange={this.onInputChange}
+                        required />
+                    <FormInput
+                        name="confirmPassword"
+                        type="password"
+                        value={this.state.confirmPassword}
+                        label="confirm password"
+                        handleChange={this.onInputChange}
+                        required />
+
+                    <div className='password-mismatch-alert'>{this.state.passwordMismatchMessage}</div>
+
+                    <div className='buttons'>
+                        <CustomButton type="submit">Sign up</CustomButton>
+                    </div>
+                </form>
+            </div>
+        );
+      }
+};
 
 export default SignUp;
