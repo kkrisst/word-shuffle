96c52338644124ccb4eb9c2c157a2358aa1f82bb
Kristóf Kövér <kristof.kover@lightware.com>
2019-09-03 15:55:25 +0200

    Screenshots added to the homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -11,6 +11,9 @@ import LandingBackground from '../../assets/dollar-gill2.jpg';
 // import { ReactComponent as LandingBackground } from '../../assets/sharon-mccutcheon';
 // import { ReactComponent as LandingBackground } from '../../assets/dollar-gill';
 
+import Screen1 from '../../assets/screen3.png';
+import Screen2 from '../../assets/screen5.png';
+
 import './homepage.styles.scss';
 
 const HomePage = ({ currentUser }) => {
@@ -51,7 +54,20 @@ const HomePage = ({ currentUser }) => {
         </div>
       </div>
       
-      <div className='app-screenshot'>PLACE A SCREENSHOT HERE</div>
+      <div className='screenshots'>
+        <div className='screenshot-container'>
+          <div className='description-container'>
+            <div className='description'>Practice your Hungarian with a huge catalogue of lessons</div>
+          </div>
+          <img className='screenshot-image' src={Screen1} alt='screenshot 1'></img>
+        </div>
+        <div className='screenshot-container'>
+          <div className='description-container'>
+            <div className='description'>Studying wid Word Shuffle is fun: drag cards to form sentences</div>
+          </div>
+          <img className='screenshot-image' src={Screen2} alt='screenshot 2'></img>
+        </div>
+      </div>
 
     </div>
   );
